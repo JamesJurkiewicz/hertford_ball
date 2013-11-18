78152c8719ab40e19bb2bf6b99e7060a5f82f49e
James Jurkiewicz <james.jurkiewicz@hertford.ox.ac.uk>
2013-11-18 14:57:53 +0000

    added css and some javascript. Menu now slides

--- myscript.js
+++ myscript.js
@@ -1,30 +1,5 @@
-function myFunction()
-{
-var x;
-var txt="";
-var person={fname:"John",lname:"Doe",age:25}; 
-
-for (x in person)
-{
-txt=txt + person[x];
-}
-
-document.getElementById("demo").innerHTML=txt;
-}
-
-$(document).ready(function($){
-    $('.dropdown-menu').click(function(e) {
-        e.stopPropagation();
-    });
-    $(".btn-group").click(function(){
-        if($(this).hasClass('open')){
-            $(".dropdown-menu").slideUp();
-        }else{
-            $(".dropdown-menu").slideDown();
-        }
-    });    
-});
-
-$(document).on('click.dropdown.data-api', function(){
-        $(".dropdown-menu").slideUp();
+$('.navbar .dropdown').hover(function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+}, function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
 });
