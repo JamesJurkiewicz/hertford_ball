afc8f9dd1334a31df2be652c10897d7f08df8b6e
James Jurkiewicz <james.jurkiewicz@hertford.ox.ac.uk>
2014-02-16 13:19:47 +0000

    added tickets change and mail list java

--- myscript.js
+++ myscript.js
@@ -3,3 +3,20 @@ $('.navbar .dropdown').hover(function() {
 }, function() {
   $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
 });
+
+
+function validateForm()
+{
+var x=document.forms["emailform"]["entry.1534874362"].value;
+var atpos=x.indexOf("@");
+var dotpos=x.lastIndexOf(".");
+if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
+  {
+  alert("Please enter a valid email address");
+  return false;
+  }
+  else
+  {
+  return submitted=true;
+  }
+}
